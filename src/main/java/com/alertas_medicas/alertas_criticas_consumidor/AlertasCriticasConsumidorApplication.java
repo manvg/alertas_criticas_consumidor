a5219deb4ece7d186b2e84c6ed2aa0c6999fbfe5
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-02-02 20:18:22 -0300

    Creación de proyecto java spring boot, con conexión a BD Oracle Cloud mediante wallet, e integración con RabbitMQ

---
+++ src/main/java/com/alertas_medicas/alertas_criticas_consumidor/AlertasCriticasConsumidorApplication.java
@@ -0,0 +1,13 @@
+package com.alertas_medicas.alertas_criticas_consumidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertasCriticasConsumidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertasCriticasConsumidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alertas_medicas/alertas_criticas_consumidor/AlertasCriticasConsumidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alertas_medicas.alertas_criticas_consumidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertasCriticasConsumidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
